64c77a53c897fa10c3560226d5f535eb258f6d2e
iqbalhossainmohon <iqbalhossainmohon3057@gmail.com>
2023-06-12 00:54:08 +0600

    manage class melete method added

--- index.js
+++ index.js
@@ -140,6 +140,12 @@ async function run() {
       res.send(result);
     })
 
+    app.delete('/classes/:id', verifyJWT, verifyAdmin, async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await classesCollection.deleteOne(query);
+      res.send(result);
+    })
 
     // class related api 
     app.get('/select', verifyJWT, async (req, res) => {
