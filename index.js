508aa268f2ae25118ee6b8cc67a02c400b555072
iqbalhossainmohon <iqbalhossainmohon3057@gmail.com>
2023-06-12 16:20:22 +0600

    create payment intent

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
+const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY)
 const port = process.env.PORT || 5000;
 
 
@@ -178,7 +179,19 @@ async function run() {
       res.send(result);
     })
 
-
+    // create payment intent 
+    app.post('/create-payment-intent', verifyJWT, async(req, res)=>{
+      const {price} = req.body;
+      const amount = price*100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card']
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      })
+    })
 
 
 
